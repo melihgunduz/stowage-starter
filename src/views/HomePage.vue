4598bf1f69dccd4cf86bc6233db4f8cfa683a48d
Melih Gündüz <melih.gunduz@hotmail.com>
2023-01-15 23:38:01 +0300

    starter-app android environment set up

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,20 +2,19 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Test</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">Test</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <strong>Let's start!!</strong>
       </div>
     </ion-content>
   </ion-page>
