2993deebdb852470b659e73664f4e41ae8ed7916
Melih Gündüz <melih.gunduz@hotmail.com>
2023-01-17 14:04:21 +0300

    genel altyapı güncellemeleri yapıldı

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router';
 
-import { IonicVue } from '@ionic/vue';
+import {IonicVue} from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -24,9 +24,9 @@ import '@ionic/vue/css/display.css';
 import './theme/variables.css';
 
 const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
+    .use(IonicVue)
+    .use(router);
+
 router.isReady().then(() => {
-  app.mount('#app');
+    app.mount('#app');
 });
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,85 +1,82 @@
 <script setup lang="ts">
-    import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-    import {onBeforeMount, ref} from "vue";
-    import {CapacitorSQLite, SQLiteConnection} from "@capacitor-community/sqlite";
-    import {square} from "ionicons/icons";
+  import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/vue';
+  import {ref} from "vue";
+  import {CapacitorSQLite, SQLiteConnection} from "@capacitor-community/sqlite";
 
-    const database = ref<any>(null);
-    const sqlite = new SQLiteConnection(CapacitorSQLite);
+  const database = ref<any>(null);
+  const sqlite = new SQLiteConnection(CapacitorSQLite);
 
 
-    const initDbTable = async () => {
-        try {
-          const isTableExists = await CapacitorSQLite.isTableExists({"table": "contacts"})
-            const CREATE_TABLE =
-                "CREATE TABLE IF NOT EXISTS test_table (" + "id INTEGER PRIMARY KEY NOT NULL,"+"first_name TEXT NOT NULL,"+"last_name TEXT NOT NULL,"+"email TEXT NOT NULL UNIQUE );";
-            const resp = await database.value?.run(CREATE_TABLE)
-            resp();
-            return true;
-        } catch (e) {
-            alert('error initializing the Database Table')
-        }
+  const initDbTable = async () => {
+    try {
+      const CREATE_TABLE =
+          "CREATE TABLE IF NOT EXISTS first_table (" + "id INTEGER PRIMARY KEY NOT NULL," + "first_name TEXT NOT NULL," + "last_name TEXT NOT NULL," + "email TEXT NOT NULL UNIQUE );";
+      await database.value?.run(CREATE_TABLE)
+      return true;
+    } catch (e) {
+      alert('error initializing the Database Table')
+      console.log(e)
+    }
+  }
+
+  const addTestUser = async () => {
+    try {
+      const resp = await database.value?.run(
+          "INSERT INTO test_table (first_name,last_name,email)" + "VALUES(?,?,?);",
+          ["Aaron", "Saunders", "aaron1@gmail.com"]
+      )
+      console.log('user added')
+      return true;
+    } catch (e) {
+      alert('error creating test user')
     }
 
-    const addTestUser = async () => {
-      try {
-        const resp = await database.value?.run(
-            "INSERT INTO test_table (first_name,last_name,email)"+"VALUES(?,?,?);",
-            ["Aaron","Saunders","aaron1@gmail.com"]
-        )
-        console.log('user added')
-        return true;
-      } catch (e) {
-        alert('error creating test user')
-      }
+  }
 
+
+  const dbConnection = async () => {
+    const isDbOpen = await sqlite.isConnection("stowage-db", false).then((value) => {
+      return value.result
+    })
+
+    if (isDbOpen) {
+      alert("veritabanı açık")
+    } else {
+      alert("veritabanı kapalı, açılıyor")
+      const db = await sqlite.createConnection('stowage-db', false, 'no-encryption', 1, false);
+      await sqlite.createConnection('stowage-db', false, 'no-encryption', 1, false)
+
+      database.value = db
     }
+  }
 
 
-    const dbConnection = async () => {
-      const isDbOpen = await sqlite.isConnection("stowage-db",false).then((value) => { return value.result})
-
-      if (isDbOpen){
-          alert("veritabanı açık")
-      }else {
-          alert("veritabanı kapalı, açılıyor")
-          const db = await sqlite.createConnection('stowage-db',false,'no-encryption',1,false);
-          await db.open();
-          database.value = db
-      }
-    }
-
-
-
-
-
-    const test = 'Test'
+  const test = 'Test'
 </script>
 
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
+<ion-page>
+  <ion-header :translucent="true">
+    <ion-toolbar>
+      <ion-title>{{ test }}</ion-title>
+    </ion-toolbar>
+  </ion-header>
+
+  <ion-content :fullscreen="true">
+    <ion-header collapse="condense">
       <ion-toolbar>
-        <ion-title>{{ test }}</ion-title>
+        <ion-title size="large">Test</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Test</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <ion-button @click="dbConnection"> Connect Database </ion-button>
-        <ion-button @click="initDbTable"> initialize table </ion-button>
-        <ion-button @click="addTestUser"> add user </ion-button>
-      </div>
-    </ion-content>
-  </ion-page>
+    <div id="container">
+      <ion-button @click="dbConnection"> Connect Database</ion-button>
+      <ion-button @click="initDbTable"> create table</ion-button>
+      <ion-button @click="addTestUser"> add user</ion-button>
+    </div>
+  </ion-content>
+</ion-page>
 </template>
-
 
 
 <style scoped>
