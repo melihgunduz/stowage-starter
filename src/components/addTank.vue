71f20f370736e38fe98ae01257e9adbfb2b4944d
Melih Gündüz <melih.gunduz@hotmail.com>
2023-01-29 21:34:35 +0300

    css added new colors

--- src/components/addTank.vue
+++ src/components/addTank.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import {IonPage, IonHeader,IonToolbar,IonButtons,IonButton,IonBackButton, IonInput, IonLabel, IonItem,IonList,IonContent, IonFooter, IonTitle, IonCard} from "@ionic/vue";
+  import {IonPage, IonHeader,IonToolbar,IonButtons,IonButton,IonBackButton, IonInput, IonLabel, IonItem,IonList,IonContent, IonFooter, IonTitle, IonCard, IonText} from "@ionic/vue";
   import {onMounted, ref} from "vue"
   import {createConn, db} from '@/helpers/dataBaseConnection'
 
@@ -50,29 +50,29 @@
     <ion-card class="ion-no-margin">
       <ion-list>
         <ion-item>
-          <ion-label position="floating" :color="tankProperties.name !== null || '' ? 'success' : 'danger'">Tank Adı</ion-label>
-          <ion-input :color="tankProperties.name !== null || '' ? 'success' : 'danger'" v-model="tankProperties.name" placeholder="Tank Adını Girin"></ion-input>
+          <ion-label position="stacked" color="inputColor">Tank Adı</ion-label>
+          <ion-input color="inputColor" v-model="tankProperties.name" placeholder="Tank Adını Girin"></ion-input>
         </ion-item>
         <ion-item>
-          <ion-label :color="tankProperties.number !== null || '' ? 'success' : 'danger'" position="floating">Tank Numarası</ion-label>
-          <ion-input :color="tankProperties.number !== null || '' ? 'success' : 'danger'" v-model="tankProperties.number" placeholder="Tank Numarasını Girin" type="number"/>
+          <ion-label color="inputColor" position="stacked">Tank Numarası</ion-label>
+          <ion-input color="inputColor" v-model="tankProperties.number" placeholder="Tank Numarasını Girin" type="number"/>
         </ion-item>
         <ion-item>
-          <ion-label :color="tankProperties.parcel !== null || '' ? 'success' : 'danger'" position="floating">Tank Parseli</ion-label>
-          <ion-input :color="tankProperties.parcel !== null || '' ? 'success' : 'danger'" v-model="tankProperties.parcel" placeholder="Tank Parselini Girin" type="number"/>
+          <ion-label color="inputColor" position="stacked">Tank Parseli</ion-label>
+          <ion-input color="inputColor" v-model="tankProperties.parcel" placeholder="Tank Parselini Girin" type="number"/>
         </ion-item>
         <ion-item>
-          <ion-label :color="tankProperties.cargo !== null || '' ? 'success' : 'danger'" position="floating">Yük</ion-label>
-          <ion-input :color="tankProperties.cargo !== null || '' ? 'success' : 'danger'" v-model="tankProperties.cargo" placeholder="Yüklenecek Yükü Yazın" type="text"/>
+          <ion-label color="inputColor" position="stacked">Yük</ion-label>
+          <ion-input color="inputColor" v-model="tankProperties.cargo" placeholder="Yüklenecek Yükü Yazın" type="text"/>
         </ion-item>
         <ion-item>
-          <ion-label :color="tankProperties.capacity !== null || '' && !isNaN(tankProperties.capacity) ? 'success' : 'danger'" position="floating">Kapasite</ion-label>
-          <ion-input :color="tankProperties.capacity !== null || '' && !isNaN(tankProperties.capacity) ? 'success' : 'danger'" v-model="tankProperties.capacity" placeholder="Metreküp cinsinden kapasite girin " type="number"/>
+          <ion-label color="inputColor" position="stacked">Kapasite</ion-label>
+          <ion-input color="inputColor" v-model="tankProperties.capacity" placeholder="Metreküp cinsinden kapasite girin " type="number"/>
         </ion-item>
       </ion-list>
     </ion-card>
   </ion-content>
-    <ion-text class="ion-padding">Not: Eklenen tank otomatik olarak 100% dolulukta eklenecektir.</ion-text>
+    <ion-text class="ion-padding" color="note">Not: Eklenen tank otomatik olarak 100% dolulukta eklenecektir.</ion-text>
   <ion-footer class="ion-padding">
     <ion-button color="success" expand="block" @click="add">Ekle</ion-button>
   </ion-footer>
